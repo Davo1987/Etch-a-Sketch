8ad4083b47fc345500d944eb55eb85817db7a453
Davo1987 <43937546+Davo1987@users.noreply.github.com>
2025-04-28 18:04:28 +0100

    Div dsiplayed using flexbox

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const container = document.querySelector('container');
+const container = document.getElementById('container');
 
 // Default number of divs to display
 const numberOfDivs = 16;
 
 // For loop to access all the divs and display on the webpage
-for (let i = 0; i < numberOfDivs; i++) {
-    const newDiv = documentt.createElement('div');
+for (let i = 0; i < numberOfDivs; i++) {    
+    const newDiv = document.createElement('div');
     newDiv.textContent = `Div ${ i + 1}`;
     container.appendChild(newDiv);
 }
