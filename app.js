2e43b141373081a46613ba11a8e1ffc1ba12afe0
Davo1987 <43937546+Davo1987@users.noreply.github.com>
2025-05-13 21:00:50 +0100

    function for grid

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const container = document.getElementById('container');
 
-// Ask user for grid size
-const numberOfDivs = prompt('Enter a number for size of grid')
-
-// For loop to create the divs and display on the webpage
-for (let i = 0; i < numberOfDivs; i++) {    
-    const newDiv = document.createElement('div');
-    newDiv.textContent = `Div ${ i + 1}`;
-    container.appendChild(newDiv);
+// Function to create grid max of 100
+function createGrid {
+    // Ask user for grid size
+    const numberOfDivs = prompt('Enter a number for size of grid');
+    if ( numberOfDivs < 100 ) {
+        // For loop to create the divs and display on the webpage
+        for (let i = 0; i < numberOfDivs; i++) {    
+            const newDiv = document.createElement('div');
+            newDiv.textContent = `Div ${ i + 1}`;
+            container.appendChild(newDiv);
+        }
+    } else {
+        alert('Grid is too big. Please tpe in less than 100');
+    }
 }
+
+createGrid();
